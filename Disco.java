28a275b75285cc0593cdd24072b5c04207f00e20
Renan santos <112184743+renan-san@users.noreply.github.com>
2026-03-31 12:30:10 -0300

    Add class header comments to Disco.java
    Added header comments with team member details and class description.

--- Disco.java
+++ Disco.java
@@ -1,4 +1,10 @@
-/*clase disco com atributos encapsulados
+/*
+Componentes:
+Victor Reis RA:10420297
+Caroline Rhe Ree Lee RA:10264791
+Renan Dos Santos Jesus RA:10748027
+
+clase disco com atributos encapsulados
  Responsável por representar um disco do jogo Torre de Hanoi.
  Cada disco possui um tamanho (valor inteiro), que define sua posição na torre.
  
