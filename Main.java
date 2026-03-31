c76ec8295aaed0ce1e8ea9267d04e09982c7bf70
Renan santos <112184743+renan-san@users.noreply.github.com>
2026-03-31 12:30:44 -0300

    Add component information to Main.java
    Added component details with student names and IDs.

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/*Componentes:
+Victor Reis RA:10420297
+Caroline Rhe Ree Lee RA:10264791
+Renan Dos Santos Jesus RA:10748027
+*/
 //cria o jogo
 //chama o método principal/menu
 public class Main {
